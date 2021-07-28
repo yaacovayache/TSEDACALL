b6e80fa1a04d991f1a1c9690808c43fef86c8ff7
jeremylzn <lznjeremy@gmail.com>
2021-07-28 14:46:05 +0300

    change home page, header and footer design

---
+++ 02_client/tsedacall/dist/tsedacall/assets/js/custom.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "290px";
+  document.getElementById('showOpen').style.display = "none";
+  document.getElementById('closeNav').style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById('showOpen').style.display = "block";
+  document.getElementById('closeNav').style.display = "none";
+}
+
+function set(name, path){
+  document.getElementById('navbarDropdownMenuLink').innerHTML = `<img src="${path}" width="20px" height="20px" alt=""> ${name} `
+}
